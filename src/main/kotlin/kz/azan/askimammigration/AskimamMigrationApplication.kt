595aa803bf5b6b50b89be9e0fe6389515142633d
Yerzhan Tulepov <yerzhan.tulepov@gmail.com>
2021-03-29 00:57:48 +0600

    Initial commit

---
+++ src/main/kotlin/kz/azan/askimammigration/AskimamMigrationApplication.kt
@@ -0,0 +1,11 @@
+package kz.azan.askimammigration
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AskimamMigrationApplication
+
+fun main(args: Array<String>) {
+	runApplication<AskimamMigrationApplication>(*args)
+}
---
+++ src/test/kotlin/kz/azan/askimammigration/AskimamMigrationApplicationTests.kt
@@ -0,0 +1,13 @@
+package kz.azan.askimammigration
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AskimamMigrationApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
